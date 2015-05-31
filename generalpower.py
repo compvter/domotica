937b34c2a64fcf684922de36ff93fe898806e458
Federico Tabbò <federico@tabbo.it>
2015-05-31 19:09:31 +0200

    Reset value if not read in 12 seconds

--- generalpower.py
+++ generalpower.py
@@ -9,31 +9,45 @@ import queue
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 blink=0
+lastquery=0
+
 
 def serialread():  #Continuous loop to read serial
-	global blink
-	while True:
-		line=ser.readline()
-		serRead=line.decode('utf-8')
+		global blink
+		while True:
+			line=ser.readline()
+			serRead=line.decode('utf-8')
 
-		if serRead == "b\r\n":					#Start of frame
-			blink = blink + 1
-			print(blink)
+			if serRead == "b\r\n":                                  #Start of frame
+				blink = blink + 1
+				print(blink)
 
+def failsafe(): #If no web reading in last 15 seconds, delete data
+		while True:
+			global lastquery
+			global blink
+			if time.time() - lastquery > 12:
+				blink=0
+				print("reset")
+			time.sleep(0.1) 
 
 class Web(object):
-	@cherrypy.expose
-	def query(self,reset=0):
-		global blink
-		count = blink #Write an actual number if reset
-		if reset == "1":
-			blink=0
-		return str(count)
-
-sreadqueue		= queue.Queue()	#Queue for packets coming from the serial
+		@cherrypy.expose
+		def query(self,reset=0):
+			global blink
+			global lastquery
+			count = blink #Write an actual number if reset
+			if reset == "1":
+				lastquery = time.time()
+				blink=0
+			return str(count)
 
 serialreadThread = threading.Thread(target=serialread)
 serialreadThread.start()
 
+failsafeThread = threading.Thread(target=failsafe)
+failsafeThread.start()
+
 cherrypy.server.socket_host = "0.0.0.0"
+cherrypy.server.socket_port = 8081
 cherrypy.quickstart(Web())
